776c0411ad532be9e288959d2531bbe366b8270c
104660 <kumars6@schneider.com>
2020-03-07 13:04:24 -0600

    deleted

--- build/static/js/3.0e204640.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpmyApp=this.webpackJsonpmyApp||[]).push([[3],{170:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return i}));var o=e(28),i=(e(6),function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.g)((function(){var t=n.innerWidth,e=n.innerHeight,i=document.elementFromPoint(t/2,e/2);if(i){var c=i.closest("ion-content");c&&c.componentOnReady().then((function(){Object(o.l)((function(){return c.scrollToTop(300)}))}))}}))}))})}}]);
-//# sourceMappingURL=3.0e204640.chunk.js.map
